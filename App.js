f3532d1d58da7431308275d983c96731f71cbdbd
Gr4y25 <minhsip210@gmail.com>
2020-11-12 20:48:18 +0700

    Update App.js

--- App.js
+++ App.js
@@ -1,5 +1,4 @@
 import React, {useEffect} from 'react';
-import {} from 'react-native';
 import SQLite from 'react-native-sqlite-storage';
 import {createStackNavigator} from '@react-navigation/stack';
 import {NavigationContainer} from '@react-navigation/native';
